765645f7fdb3cafa1c35bf57516451778a6b5d5f
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2023-02-02 13:39:43 +0700

    update Footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => (
           Metaversus
         </h4>
         <p className="font-normal text-[14px] text-white opacity-50">
-          Copyright © 2021 - 2022 Metaversus. All rights reserved.
+          Copyright © 2023 Metaversus. All rights reserved.
         </p>
         <div className="flex gap-4">
           {
